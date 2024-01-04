662c27ef185f179d2cd3ba64031981e20236b231
DenzelWashington <denzelwashingtonsantana@gmail.com>
2024-01-04 05:15:57 -0300

    Update Textfield

--- client/src/components/Textfield/index.tsx
+++ client/src/components/Textfield/index.tsx
@@ -1,49 +1,19 @@
-import { InputHTMLAttributes, useState } from 'react'
+import { InputHTMLAttributes } from 'react'
 import * as S from './styles'
 
 export type TextFieldProps = {
-  onInputChange?: (value: string) => void
-  icon?: React.ReactNode
-  disabled?: boolean
   error?: string
-  label?: string
-  initialValue?: string
+  icon?: React.ReactNode
 } & InputHTMLAttributes<HTMLInputElement>
 
-const TextField = ({
-  label,
-  name,
-  initialValue = '',
-  disabled,
-  error,
-  onInputChange,
-  icon
-}: TextFieldProps) => {
-  const [value, setValue] = useState(initialValue)
-
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newValue = e.currentTarget.value
-    setValue(newValue)
-
-    !!onInputChange && onInputChange(newValue)
-  }
-
-  return (
-    <S.Wrapper disabled={disabled} error={!!error}>
-      {!!label && <S.Label htmlFor={name}>{label}</S.Label>}
-      <S.InputWrapper>
-        {!!icon && <S.Icon>{icon}</S.Icon>}
-        <S.Input
-          type="text"
-          onChange={onChange}
-          value={value}
-          disabled={disabled}
-          name={name}
-        />
-      </S.InputWrapper>
-      {!!error && <S.Error>{error}</S.Error>}
-    </S.Wrapper>
-  )
-}
+const TextField = ({ error, icon, ...props }: TextFieldProps) => (
+  <S.Wrapper>
+    <S.InputWrapper>
+      {!!icon && <S.Icon>{icon}</S.Icon>}
+      <S.Input type="text" {...props} />
+    </S.InputWrapper>
+    {!!error && <S.Error>{error}</S.Error>}
+  </S.Wrapper>
+)
 
 export default TextField
